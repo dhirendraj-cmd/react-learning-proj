27a23a5aedec3742faa63ae129a5690e11947c58
dj221ai <djha5051@gmail.com>
2025-10-19 20:58:23 +0530

    created context and provder

--- contextAPI/themecontext/src/App.js
+++ contextAPI/themecontext/src/App.js
@@ -2,15 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/theme';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [themeMode, setThemeMode] = useState("lightgray")
+
+  const darkMode = () => {
+    setThemeMode("dark");
+  }
+
+  const lightMode = () => {
+    setThemeMode("lightgray");
+  }
+
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("lightgray", 'dark');
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode])
+  
+
   return (
-    <div>
-      <Navbar/>
-      <main className="main-content">
-        <Login/>
-      </main>
-    </div>
+    <ThemeProvider value={{themeMode, darkMode, lightMode}}>
+      <div>
+        <Navbar/>
+        <main className="main-content">
+          <Login/>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- contextAPI/themecontext/src/components/ThemeButton.jsx
+++ contextAPI/themecontext/src/components/ThemeButton.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './ThemeButton.css'
+import useTheme from '../context/theme'
 
 
 const ThemeButton = () => {
+  const {themeMode, lightMode, darkMode} = useTheme()
+
+  const onChangeBtn = (e) => {
+    const darkModeStatus = e.currentTarget.checked
+        if (darkModeStatus) {
+            darkMode()
+        } else {
+            lightMode()
+        }
+  }
+
   return (
-    <div className='theme-button'>DarkMode</div>
+    <div className='theme-button'>
+      <input
+                type="checkbox"
+                value=""
+                className="sr-only peer"
+                onChange={onChangeBtn}
+                checked={themeMode=== "dark"}
+        />
+        <span>Toggle</span>
+    </div>
   )
 }
 
